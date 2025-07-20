58f91d7d485623bd61a6174acd9eb3a91e7f9f44
marcossoares-dev <55550244+marcossoares-dev@users.noreply.github.com>
2025-07-20 20:14:42 -0300

    main

---
+++ main.py
@@ -0,0 +1,17 @@
+idade: int = 33
+altura: float = 1.86
+nome: str = 'Marcos'
+is_estudante: bool = True
+
+
+produto: str = "sapato"
+produto2: str = "camiseta"
+produto3: str = "videogame"
+
+produtos: list = []
+
+produtos.append(produto)
+produtos.append(produto2)
+produtos.append(produto3)
+
+print(produtos)
