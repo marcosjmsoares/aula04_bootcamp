97205e0ba7d8f6f45527c716561dc3b20eb66d74
Marcos Soares <marcosjmsoares@gmail.com>
2025-08-26 18:35:46 -0300

    ajuste exercicios

--- exercicios2.py
+++ exercicios2.py
@@ -136,13 +136,13 @@
 # 15. Contagem de Frequência de Itens
 # Objetivo: Dada uma string, contar a frequência de cada caractere usando um dicionário.
 
-texto = "engenharia de dados"
-frequencia = {}
+# texto = "engenharia de dados"
+# frequencia = {}
 
-for caractere in texto:
-    if caractere in frequencia:
-        frequencia[caractere] += 1
-    else:
-        frequencia[caractere] = 1
+# for caractere in texto:
+#     if caractere in frequencia:
+#         frequencia[caractere] += 1
+#     else:
+#         frequencia[caractere] = 1
 
-print(frequencia)
+# print(frequencia)
